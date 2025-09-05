089117053340a153ed10f8ce2e0957fd1bb36142
Himalisharma <himaliisharma@gmail.com>
2025-09-05 22:35:45 +0530

    fix: update next.config.mjs for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 // next.config.mjs
-const isProd = process.env.NODE_ENV === 'production';
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',                       // Required for static export
-  basePath: isProd ? '/himali-portfolio' : '',
-  assetPrefix: isProd ? '/himali-portfolio/' : '',
-  images: { unoptimized: true },          // Disable image optimization for static export
+  
+  images: {
+    unoptimized: true, // optional, can keep or remove
+  },
 };
 
 export default nextConfig;
